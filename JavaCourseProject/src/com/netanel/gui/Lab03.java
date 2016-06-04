ffb8cb439c68f120d89f50156d7ec44e94915700
netanel <netanel@MINI>
2016-06-05 00:51:43 +0300

    gui lab 03

--- JavaCourseProject/src/com/netanel/gui/Lab03.java
+++ JavaCourseProject/src/com/netanel/gui/Lab03.java
@@ -6,49 +6,85 @@ import javax.swing.*;
 
 public class Lab03 {
 	public static void main(String[] args) {
-		JFrame f = new JFrame("Blah");
+		// Create Frame
+		JFrame f = new JFrame("Connection User Interface");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setVisible(true);
 		f.setLocation(200, 200);
-		//////////
+		// Create Panels 
 		JPanel labelPanel = new JPanel(new BorderLayout());
-		labelPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-		JPanel checkBoxPanel = new JPanel();
-		checkBoxPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-		JPanel radioPanel = new JPanel();
-		radioPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+		JPanel checkBoxPanel = new JPanel(new GridLayout(3,1));
+		JPanel radioPanel = new JPanel(new GridLayout(3, 1));
 		JPanel radioAndCheckBoxPanel = new JPanel(new BorderLayout());
-		radioAndCheckBoxPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-		JPanel buttonsPanel = new JPanel();
+		JPanel buttonsPanel = new JPanel(new GridLayout(3, 1));
 		JPanel comboPanel = new JPanel();
-		//////////
-		
+
+		// Top Label		
 		JLabel label = new JLabel("Connection User Interface");
-		labelPanel.add(label);
-		/////////
+		label.setFont(new Font("Arial", Font.BOLD + Font.ITALIC, 25));
+		labelPanel.add(label, BorderLayout.WEST);
+		// Check-boxes
 		JCheckBox chk1 = new JCheckBox("Proxy");
 		JCheckBox chk2 = new JCheckBox("Firewall");
 		JCheckBox chk3 = new JCheckBox("SSL Enabled");
 		
+		chk1.setToolTipText("Enable Proxy");
+		chk2.setToolTipText("Enable Firewall");
+		chk3.setToolTipText("Enable SSL");
+		
 		checkBoxPanel.add(chk1);
 		checkBoxPanel.add(chk2);
 		checkBoxPanel.add(chk3);
-		//////////
-		JRadioButtonMenuItem radio1 = new JRadioButtonMenuItem("Shared");
-		JRadioButtonMenuItem radio2 = new JRadioButtonMenuItem("Not Shared");
+		// Radio Buttons
+		JLabel radioLabel = new JLabel("Files");
+		JRadioButton radio1 = new JRadioButton("Shared");
+		JRadioButton radio2 = new JRadioButton("Not Shared");
 		
+		ButtonGroup radioGroup = new ButtonGroup();
+		radioGroup.add(radio1);
+		radioGroup.add(radio2);
+		
+		radio1.setToolTipText("Share Files");
+		radio2.setToolTipText("Do Not Share Files");
+		
+		radioPanel.add(radioLabel);
 		radioPanel.add(radio1);
 		radioPanel.add(radio2);
-		/////////////
+		// Buttons
+		Icon connectedIcon = new ImageIcon("files/icons/connected.png");
+		Icon disconnectedIcon = new ImageIcon("files/icons/disconnected.png");
+		Icon awayIcon = new ImageIcon("files/icons/away.png");
 		
-		radioAndCheckBoxPanel.add(checkBoxPanel);
-		radioAndCheckBoxPanel.add(radioPanel);
+		JButton btnConnect = new JButton("Connect", connectedIcon);
+		JButton btnDisconnect = new JButton("Disconnect", disconnectedIcon);
+		JButton btnAway = new JButton("Away", awayIcon);
+		
+		btnConnect.setToolTipText("Connect");
+		btnConnect.setHorizontalAlignment(SwingConstants.LEFT);
+		btnDisconnect.setToolTipText("Disconnect");
+		btnDisconnect.setHorizontalAlignment(SwingConstants.LEFT);
+		btnAway.setToolTipText("Away");
+		btnAway.setHorizontalAlignment(SwingConstants.LEFT);
+		
+		buttonsPanel.add(btnConnect);
+		buttonsPanel.add(btnDisconnect);
+		buttonsPanel.add(btnAway);
+		// Combo Box
+		JLabel comboLabel = new JLabel("Choose User");
+		String[] comboStr = {"User1", "User2", "User3"};
+		JComboBox<String> combo = new JComboBox<>(comboStr);
+		
+		comboPanel.add(comboLabel);
+		comboPanel.add(combo);
+		
+		// Add all Panels to Frame
+		radioAndCheckBoxPanel.add(checkBoxPanel, BorderLayout.NORTH);
+		radioAndCheckBoxPanel.add(radioPanel, BorderLayout.SOUTH);
 		f.add(labelPanel, BorderLayout.NORTH);
 		f.add(radioAndCheckBoxPanel, BorderLayout.WEST);
-		
-		
-		f.pack();
-		
-		
+		f.add(buttonsPanel, BorderLayout.CENTER);
+		f.add(comboPanel, BorderLayout.EAST);
+				
+		f.pack();		
 	}
 }
